a10ec097ff703c522e8cf290c3098ef708ec5f41
MirTunio <33496277+MirTunio@users.noreply.github.com>
2017-12-05 14:35:48 -0800

    Update Bees2.py

--- Bees - Importance of genetic diversity/Bees2.py
+++ Bees - Importance of genetic diversity/Bees2.py
@@ -1,18 +1,21 @@
 #Bees
+'''
+True stability is borne out of the interplay of opposing forces. Too much homogeniety breeds instability.
+'''
 import numpy as np
 from matplotlib import pyplot as plt
 
-HiveT = 30
+HiveT = 39 #Starting temperature
 
 class Bees:
     def __init__(self,dist):
         self.dist = dist
         if self.dist:
-            self.gtI = np.random.normal(loc = 30.12311, scale = 6) #genetic threshold
+            self.gtI = np.random.normal(loc = 30.12311, scale = 6) #Randomly distributed ideal temperatures about the true hive ideal
         else:
-            self.gtI = 30.12311
+            self.gtI = 30.12311 #All ideal temperatures exactly the hive ideal temperature
 
-    def HorC(self,HT):
+    def HorC(self,HT): #Each bees internal thermostat, HT is the Hive Temperature
         self.heat = False
         self.cool = False
         self.stay = True
@@ -33,28 +36,25 @@ class Bees:
 #SET ranno to True for genetic variety
 #OBSERVE the lack of stability when there is lack of genetic variety
 
-def trial(iterations,ranno,HiveT): #look at the amount of bees cooling and heating, and then cahnge the Hivetemeprature accrodingl
-    BeeNum = 100.
-    Tray = np.array([])
-    Hive = [Bees(ranno) for i in np.arange(BeeNum)]
+def trial(iterations,ranno,HiveT): #Look at the amount of bees cooling and heating, and then change the hive temperature accordingly
+    BeeNum = 100. #The number of bees
+    Tray = np.array([]) #Holds the historical temperature of the hive
+    Hive = [Bees(ranno) for i in np.arange(BeeNum)] #Fills the hive with bees
 
-    for i in np.arange(iterations):
+    for i in np.arange(iterations): #See what each bee's thermostat feels about the temperature
         for bee in Hive:
             bee.HorC(HiveT)
 
-        Heaters = [bee.heat for bee in Hive]
-        Coolers = [bee.cool for bee in Hive]
+        Heaters = [bee.heat for bee in Hive] #List of bees heating the hive
+        Coolers = [bee.cool for bee in Hive] #List of bees cooling the hive   
 
-        #print(Heaters, Coolers,HiveT)
+        heatp = np.count_nonzero(Heaters) #The number of bees heating the hive
+        coolp = np.count_nonzero(Coolers) #The number of bees cooling the hive
 
-        heatp = np.count_nonzero(Heaters)
-        coolp = np.count_nonzero(Coolers)
-
-        #print(heatp,coolp)
-        HiveT = HiveT + heatp/BeeNum - coolp/BeeNum
-        Tray = np.append(Tray,HiveT)
-        #print(HiveT)
-    plt.plot(Tray)
+        HiveT = HiveT + heatp/BeeNum - coolp/BeeNum #Adjust the hive temperature accordingly
+        Tray = np.append(Tray,HiveT) #Add temperature to log
+        
+    plt.plot(Tray) # The following lines plots the temperature log
     plt.xlabel('Time')
     plt.ylabel('Temperature')
     plt.show()
