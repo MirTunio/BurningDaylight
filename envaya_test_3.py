281d4263b4a65a550506dc56b64968a1ccca7b94
MirTunio <33496277+MirTunio@users.noreply.github.com>
2019-05-22 21:27:57 +0500

    Add files via upload

---
+++ envaya_test_3.py
@@ -0,0 +1,78 @@
+"""
+TUNIO 2019
+tuniomurtaza@gmail.com
+
+Python Envaya Wiki Client
+"""
+
+from flask import Flask, request, jsonify
+import Wikihelper
+import time
+import json
+
+app = Flask(__name__)
+
+@app.route('/echo', methods=['GET', 'POST', 'OPTIONS'])
+def echo():
+    """ takes the message forwarded by the envaya client on the phone and sends
+    it back to the sender, exactly"""
+    form_in = request.form
+    ACTION_IN = form_in['action']
+    print("ACTION IN: " + form_in['action'])
+    print("")
+    
+    if ACTION_IN == 'incoming':
+        FROM = form_in['from']
+        MSG = form_in['message']
+        print("recieved message from {}:".format(FROM)) #.copy() use
+        print(MSG)
+        print("replying with:")
+        sending = jsonify({'events':[{'event': 'send', 'messages':[{'id':'123123','to':FROM,'message': MSG}]}]})
+        print(sending.get_data())
+        print("")
+        return sending
+    
+    elif ACTION_IN == 'send_status':
+        print('recieved STATUS: ' + form_in['log'])
+        return jsonify({'events':''} )
+
+def get_wiki(body, from_number):
+    try:
+        return Wikihelper.wiki(body, from_number)    
+    except:
+        return "An error occured, try something different!"
+    
+@app.route('/sms', methods=['GET', 'POST', 'OPTIONS'])
+def wiki():
+    """ this function takes the incoming message, sends the body of the message
+    to the Wikihelper module, and sends the result back to the Envaya app using
+    the appropriate formatting"""
+    
+    form_in = request.form
+    ACTION_IN = form_in['action']
+    print("")
+    print("*****")
+    print("ACTION IN: " + form_in['action'])
+    print("")
+    
+    if ACTION_IN == 'incoming':
+        FROM = form_in['from']
+        MSG = form_in['message']
+        print("Recieved message from {}:".format(str(FROM)))
+        print('"'+MSG+'"')
+        reply = get_wiki(MSG,FROM)
+        sending = jsonify({'events':[{'event': 'send', 'messages':[{'id':FROM+'::'+str(time.time()),'to':FROM,'message': reply}]}]})
+        print("")
+        print("Replying with:")
+        print('"'+reply+'"')
+        print("")
+        return sending
+    
+    elif ACTION_IN == 'send_status':
+        print('FULL STATUS: ')
+        print(json.dumps(form_in, indent=4))
+        print("")
+        return jsonify({'events':''} ) # Properly formatted response with no content, just so envaya doesn't freak out
+
+if __name__ == "__main__":
+    app.run(debug=True)
